30435ac033b352ac049fbfa2f0418cdf4aa9390f
Rafael Santin <rafael-sg@live.com>
2019-03-06 22:32:24 -0300

    fix emmit client

--- client/src/api.js
+++ client/src/api.js
@@ -1,5 +1,5 @@
 import openSocket from 'socket.io-client';
-const socket = openSocket('http://localhost:3002');
+const socket = openSocket('https://streamlabslast.herokuapp.com/');
 
 function subscribeToTimer(cb) {    
     socket.on('someName', teste => cb(null, teste))
